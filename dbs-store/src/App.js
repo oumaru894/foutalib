8190834eda31c2e69dfbb2f9d02a4dc1e132e17a
oumaru894 <bahoumar654321@gmail.com>
2024-11-24 06:31:01 +0000

    building

--- dbs-store/src/App.js
+++ dbs-store/src/App.js
@@ -1,5 +1,5 @@
 import React from 'react';
-import { BrowserRouter as Router, Route, Routes, useLocation } from 'react-router-dom';
+import { BrowserRouter as Router, Route, Routes, useLocation, Switch } from 'react-router-dom';
 import Navbar from './components/Navbar';
 import Footer from './components/Footer';
 import BestSellers from './components/bestSeller/BestSellers';
